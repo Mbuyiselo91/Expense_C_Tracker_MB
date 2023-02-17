c0d25e9859db05078cdd44a9cf5f16f207897572
Mbuyiselo91 <mbuyisolo@gmail.com>
2023-02-17 12:55:26 +0200

    fixed the mistake alert, show if mistake alert display add name

--- app.js
+++ app.js
@@ -29,10 +29,8 @@ function displayShow(){
 
 function uniqueId(){
     return Math.round(Math.random() * 1000000);
+    
 }
-
-
-// DRY - Do not repeat yourself
 
 function onAddIncomeClick(){
     addTransaction(nameInputEl.value, amountInputEl.value, 'income');
@@ -43,17 +41,23 @@ function addTransaction(name, amount, type){
     var amount = amountInputEl.value;
 
 
-    if (name.value == '' && amount.value == '') {
+    if (name !='' && amount !='') {
+       // alert("Please add name");
         var transaction =  {id: uniqueId(), name: name, amount: parseInt(amount), type: type};
         data.transactions.push(transaction);
         updateState();
+
     }
-    else {
-        alert("Please add input");
+    else
+    {
+        alert("Please add name");
+        
     }
+    
 
     nameInputEl.value = '';
     amountInputEl.value = '';
+    
 }
 
 function onAddExpenseClick(){
@@ -142,7 +146,7 @@ function render(){
 
         btnEl = document.createElement('button');
         btnEl.setAttribute('data-id', item.id);
-        //Boostrap the icons last for delete icons table 
+        //Boostrap the icons last for delete icons table,row 
         btnEl.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 32 32"><path fill="none" stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M28 6H6l2 24h16l2-24H4m12 6v12m5-12l-1 12m-9-12l1 12m0-18l1-4h6l1 4"/></svg>`;
 
         btnEl.addEventListener('click', onDeleteClick);
@@ -152,6 +156,4 @@ function render(){
         transactionEl.appendChild(containerEl);
     }
 }
-
-
 displayShow();
