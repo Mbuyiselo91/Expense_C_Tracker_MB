58e01fafcc897bfd82ba04b0963149235e666eee
Mbuyiselo91 <mbuyisolo@gmail.com>
2023-02-17 13:12:33 +0200

    put the write commented font awesome

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ function render(){
 
         btnEl = document.createElement('button');
         btnEl.setAttribute('data-id', item.id);
-        //Boostrap the icons last for delete icons table,row 
+        //font awesome the icons last for delete icons table,row 
         btnEl.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 32 32"><path fill="none" stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M28 6H6l2 24h16l2-24H4m12 6v12m5-12l-1 12m-9-12l1 12m0-18l1-4h6l1 4"/></svg>`;
 
         btnEl.addEventListener('click', onDeleteClick);
